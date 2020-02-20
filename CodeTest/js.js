199d58ab831b2d0fd1123f59aef227e4ac84cb99
TWenzel1 <48931883+TWenzel1@users.noreply.github.com>
2020-02-19 23:48:44 -0600

    add datafirst

---
+++ CodeTest/js.js
@@ -0,0 +1,20 @@
+
+let jump = 5;
+let joe = "hello";
+let joel = [];
+
+function hello() {
+    console.log(joe);
+};
+console.log("enter a number");
+
+
+hello();
+hello1();
+
+
+
+
+
+
+console.log(jump);
